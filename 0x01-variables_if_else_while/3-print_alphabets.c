8b96508414372444b048e0f69871dcda2c138bb6
Nzuki2022 <119017646+Nzuki2022@users.noreply.github.com>
2022-12-21 17:27:11 +0300

    Add files via upload

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(toupper(x));
+	}
+	putchar('\n');
+	return (0);
+}
